753496864e607d414b5e29d74f09c8761bf5b71e
AngieBrilliance <angie@codeforprogress.org>
2014-04-30 10:36:17 -0400

    Added the first game!

---
+++ ABGameStation.py
@@ -0,0 +1,58 @@
+# ABGameStation.py9
+# A trivia game that asks multiple choice questions and tell the user if they are right or wrong.
+# It will have five questions.
+
+# Welcome screen
+print "\n\n\n\n\n\n\nMe and You, Angry Black Women, and yo momma too. \n\n\n Name that really dope Black Radical Feminist Organizer!"
+
+
+
+# Section 1. The First Question
+print "\n\n\n\n\n\n\n Question 1. "
+print "She said: Strong People don't need strong leaders."
+print "\n(a) Audre Lorde \n(b) Angela Davis \n(c) Ella Baker \n(d) Fannie Lou Hamer"
+
+answer = raw_input("Your answer:") #Asks the user for their response and saves it as answer
+
+if answer=="c": #Checks to see if answer is correct. If it is, say thats correct! If it isn't, say so.
+	print "She did that!"
+else: print "So what you're saying is you're not a feminist...\n"
+print "\n\nThe answer was The honorable Ella Baker. She believed in group models of leadership."
+
+
+# Section 2. The Second Question
+print "\n\n\n\n\n\n\n Question 2. "
+print "She said: It is our duty to fight for our freedom. It is our duty to win.\nWe must love eachother and protect eachother. We have nothing to lose but our chains."
+print "\n(a) Assata Shakur \n(b) Barbera Ransby \n(c) Cecile Fatiman \n(d) Elaine Brown"
+
+answer = raw_input("Your answer:") #Asks the user for their response and saves it as answer
+
+if answer=="a": #Checks to see if answer is correct. If it is, say thats correct! If it isn't, say so.
+	print "She said that!"
+else: print "So now you dont believe in self-care and community accountability...interesting.\n"
+print "\n\nThe answer was the omnipotent Assata Shakur."
+
+
+# Section 3. The Third Question
+print "\n\n\n\n\n\n\n Question 3. "
+print "Loss of social standing is an ever-present threat for individuals whose social acceptance is based on behavioral traits rather than unconditional human value."
+print "\n(a) Beyonce \n(b) Nikki Giovanni \n(c) Chimimanda Ngozi Adichie \n(d) Melissa Harris Perry"
+
+answer = raw_input("Your answer:") #Asks the user for their response and saves it as answer
+
+if answer=="d": #Checks to see if answer is correct. If it is, say thats correct! If it isn't, say so.
+	print "She surely said that!"
+else: print "Youve gotta do better.\n"
+print "\n\nThe answer was actually Melissa Harris Perry."
+
+# Section 4. The Fourth Question
+print "\n\n\n\n\n\n\n Question 4. "
+print "\n Master's tools will never dismantle master's house."
+print "\n(a) Audre Lorde \n(b) Betty Shabazz \n(c) Harriet Tubman \n(d) Sojourner Truth"
+
+answer = raw_input("Your answer:") #Asks the user for their response and saves it as answer
+
+if answer=="a": #Checks to see if answer is correct. If it is, say thats correct! If it isn't, say so.
+	print "You beat this quiz, Now let's talk about your politics aroung Black Radical feminism!"
+else: print "Youve just gotta do better.\n"
+print "\n\nThe answer was Audre Lorde."
